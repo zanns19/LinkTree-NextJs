aa51d809f53bd761075f0ec0dde3af41b6c790b0
Zuhair <zuhairanns@gmail.com>
2026-06-29 16:24:33 +0500

    all functionalities are added successfully

--- app/page.js
+++ app/page.js
@@ -1,10 +1,39 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+  const [text, setText] = useState("")
+
+  
+  const createTree = () => { 
+    
+    router.push(`/generate?handle=${text}`)
+  }
   return (
-    <>
-   <div className="bg-black min-h-screen">sksjhfk</div>
-   <div className="bg-red-700 min-h-screen">sksjhfk</div>
-    </>
+    <main>
+      <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
+    <div className="flex justify-center flex-col ml-[10vw] gap-3">
+      <p className="text-yellow-300 font-bold text-4xl">Everything you </p>
+      <p className="text-yellow-300 font-bold text-4xl">are. In one,</p>
+      <p className="text-yellow-300 font-bold text-4xl">simple link in bio.</p>
+      <p className="text-yellow-300 text-lg my-4">Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+      <div className="input flex gap-2">
+        <input value={text} onChange={(e)=> setText(e.target.value)} className="px-2 py-2 focus:outline-green-800 outline-green-500 border border-green-400 text-white rounded-md" type="text" placeholder="Enter your Handle" />
+        <button onClick={()=> createTree()} className="bg-pink-300 rounded-full px-4 py-4 font-semibold">Claim your Bittree</button>
+      </div>
+    </div>
+    <div className="flex items-center justify-center flex-col mr-[10vw]">
+      <img src="/home.png" alt="homepage image" />
+
+    </div>
+      </section>
+      <section className="bg-red-700 min-h-[100vh]">
+ 
+      </section>
+    </main>
   );
 }
